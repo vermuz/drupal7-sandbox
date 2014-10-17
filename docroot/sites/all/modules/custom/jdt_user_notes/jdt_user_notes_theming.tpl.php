3b16d38cb4efd66f18cb599e47bd5956a53a3dfa
Mani <chaudhry@appnovation.com>
2014-10-17 14:12:02 -0400

    Add Template for tab

---
+++ docroot/sites/all/modules/custom/jdt_user_notes/jdt_user_notes_theming.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+  * @file
+  * Template file for tabs
+  * Available custom variables:
+  * -
+  * -
+  */
+?>
+
+<!-- Tab template -->
+<div class="note-container">
+  <div class="note-text">NOTE_TEXT</div>
+  <div class="node-title">NODE_TITLE</div>
+  <div class="node-author">NODE_AUTHOR</div>
+</div>
+<!-- /Tab template -->
