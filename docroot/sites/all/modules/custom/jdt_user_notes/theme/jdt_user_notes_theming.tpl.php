cba7708265592524035b075fa08dbd65cc98e920
Mani <chaudhry@appnovation.com>
2014-10-17 16:17:17 -0400

    Fixing variables being passed to theme to show values

--- docroot/sites/all/modules/custom/jdt_user_notes/theme/jdt_user_notes_theming.tpl.php
+++ docroot/sites/all/modules/custom/jdt_user_notes/theme/jdt_user_notes_theming.tpl.php
@@ -4,12 +4,12 @@
   * Template file for tabs
   * Available custom variables:
   */
-
+  //dpm($variables);
 ?>
 <!-- Tab template -->
 <div class="note-container">
-  <div class="note-text"><?php print $NOTE_TEXT; ?></div>
-  <div class="node-title"><?php print $NODE_TITLE; ?></div>
-  <div class="node-author"><?php print $NODE_AUTHOR; ?></div
+  <div class="note-text">Notes Related: <?php print $NOTE_TEXT; ?></div>
+  <div class="node-title">Node Title: <?php print $NODE_TITLE; ?></div>
+  <div class="node-author">Node Author: <?php print $NODE_AUTHOR; ?></div
 </div>
 <!-- /Tab template -->
